03a2d88057cc7e92961f13f8d3c864ef9ed5180e
zaldi <akurizaldirv@gmail.com>
2024-04-24 10:35:27 +0700

    [INIT] initial project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='vh-100 bg-danger'>
+        Warung Makan Bahari
+      </div>
+    </>
+  )
+}
+
+export default App
