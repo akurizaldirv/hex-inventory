7df2df2e75c516139e3437b0867bcb514200886b
zaldi <akurizaldirv@gmail.com>
2024-04-24 19:48:21 +0700

    [FIX] fix log dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -24,18 +24,6 @@ export default class Dashboard extends Component {
 					Suasana hangat dan ramah membuat pengunjung betah menikmati
 					hidangan lezat di sini.
 				</p>
-				{/* <h1 className='fs-2'>Dashboard</h1>
-        <div className="d-flex gap-3"> */}
-
-				{/* <div className='rounded-4 bg-secondary fst-italic text-center px-4 pb-4'>
-                <div className='fw-bold m-0' style={{fontSize: 84}}>8</div>
-                <div className='m-0'>total menu</div>
-            </div>
-            <div className='rounded-4 bg-primary fst-italic text-center px-4 pb-4'>
-                <div className='fw-bold m-0' style={{fontSize: 84}}>8</div>
-                <div className='m-0'>total table</div>
-            </div> */}
-				{/* </div> */}
 			</div>
 		);
 	}
